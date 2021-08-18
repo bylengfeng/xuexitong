b1febe330f9a1bc2c9aa980d64a490344dac3c05
bylengfeng <49505986+bylengfeng@users.noreply.github.com>
2021-08-18 21:41:53 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# -
+##修改config.js 替换为你的域名
+//需要搭建运营请修改index.js 329行  search_xq.js 177 注释替换即可
+// 源码作者vx：luwenjie712 20218/18开源
+// csdn博客：https://blog.csdn.net/qq_35230125/article/details/119788899
---
+++ config.js
@@ -0,0 +1,13 @@
+
+var Url = 'https://ev.airjpg.com';   //后端域名
+var url = 'https://ai.airjpg.com';   //数据域名
+var api = {
+ url_a: Url,
+ url_s: url,
+  }
+
+module.exports = api    
+
+// 需要搭建运营请修改index.js 329行  search_xq.js 177 注释替换即可
+// 源码作者vx：luwenjie712 20218/18开源
+// csdn博客：https://blog.csdn.net/qq_35230125/article/details/119788899
